dfa014ccca90ecf03aae74d889cc53ff8267d8e5
samantha <samanthap8675@gmail.com>
2024-02-05 17:31:45 -0500

    firebase

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,30 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCzMKMwqlqsjc57IQaUSTXkns2rpHNGZ40",
+  authDomain: "edusync-651c2.firebaseapp.com",
+  projectId: "edusync-651c2",
+  storageBucket: "edusync-651c2.appspot.com",
+  messagingSenderId: "881867413603",
+  appId: "1:881867413603:web:73bebf4ced1f5eef02c056",
+  measurementId: "G-V4HLZBESDV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 function App() {
   const [count, setCount] = useState(0)
 
