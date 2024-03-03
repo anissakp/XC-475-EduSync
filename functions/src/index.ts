a321d6e66e4dcd65cfa83479061dabfb212a91ea
zsugino <124764743+zsugino@users.noreply.github.com>
2024-03-03 17:20:30 -0500

    redirect url changed

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -35,8 +35,8 @@ const getActualToken = async (code:any) => {
     console.log("this is requestbody", requestBody);
 
     const response = await fetch(
-      // `https://ec2-3-87-255-160.compute-1.amazonaws.com/learn/api/public/v1/oauth2/token?code=${code}&redirect_uri=https://edusync-e6e17.web.app/dashboard`,
-      `https://ec2-3-87-255-160.compute-1.amazonaws.com/learn/api/public/v1/oauth2/token?code=${code}&redirect_uri=http://localhost:5173/dashboard`,
+      `https://ec2-3-87-255-160.compute-1.amazonaws.com/learn/api/public/v1/oauth2/token?code=${code}&redirect_uri=https://edusync-e6e17.web.app/dashboard`,
+      // `https://ec2-3-87-255-160.compute-1.amazonaws.com/learn/api/public/v1/oauth2/token?code=${code}&redirect_uri=http://localhost:5173/dashboard`,
       {
         body: requestBody,
         method: "POST",
@@ -63,8 +63,8 @@ export const getConnection = onRequest({cors: true}, async (req, res)=>{
   try {
     // console.log("clientid", process.env.CLIENT_ID);
     const response = await fetch(
-      // `https://ec2-3-87-255-160.compute-1.amazonaws.com/learn/api/public/v1/oauth2/authorizationcode?redirect_uri=https://edusync-e6e17.web.app/dashboard&response_type=code&client_id=${process.env.CLIENT_ID}&scope=read&state=DC1067EE-63B9-40FE-A0AD-B9AC069BF4B0`,
-      `https://ec2-3-87-255-160.compute-1.amazonaws.com/learn/api/public/v1/oauth2/authorizationcode?redirect_uri=http://localhost:5173/dashboard&response_type=code&client_id=${process.env.CLIENT_ID}&scope=read&state=DC1067EE-63B9-40FE-A0AD-B9AC069BF4B0`,
+      `https://ec2-3-87-255-160.compute-1.amazonaws.com/learn/api/public/v1/oauth2/authorizationcode?redirect_uri=https://edusync-e6e17.web.app/dashboard&response_type=code&client_id=${process.env.CLIENT_ID}&scope=read&state=DC1067EE-63B9-40FE-A0AD-B9AC069BF4B0`,
+      // `https://ec2-3-87-255-160.compute-1.amazonaws.com/learn/api/public/v1/oauth2/authorizationcode?redirect_uri=http://localhost:5173/dashboard&response_type=code&client_id=${process.env.CLIENT_ID}&scope=read&state=DC1067EE-63B9-40FE-A0AD-B9AC069BF4B0`,
       {
         headers: {
           "Content-Type": "form/urlencoded",
