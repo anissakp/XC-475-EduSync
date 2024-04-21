139bb2f05732f8c440892e1e21174e77df26e0d0
emily <emilydoh@bu.edu>
2024-04-21 12:26:54 -0400

    progress bar implemented and design tweaked - should it be all assignments or just those due this week?

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -2,20 +2,33 @@
 import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge';
 
 
-export default function ProgressBar() {
+
+interface Props {
+  weeklyPercent: number
+};
+
+
+const ProgressBar : React.FC<Props> = ({weeklyPercent}) => {
+
   return <div className="col-span-1 h-96 md:h-auto md:col-span-2 rounded-tl-lg rounded-tr-lg bg-gradient-custom3">
     <h3 className="bg-custom-gray2 px-5 py-2 rounded-tl-lg rounded-tr-lg">Progress Bar</h3>
     <div className="flex justify-center items-center h-3/4 p-2">
       <Gauge
-      width={200} 
-      height={200} 
-      value={75}
-      startAngle={0}
-      endAngle={360}
-      innerRadius="75%"
-      outerRadius="100%"
+        width={200} 
+        height={200} 
+        cornerRadius="50%"
+        value={weeklyPercent}
+        startAngle={0}
+        endAngle={360}
+        innerRadius="75%"
+        outerRadius="100%"
+        text={
+          ({value}) => `${value}%`
+        }
       />
     </div>
 
   </div>
 }
+
+export default ProgressBar
--- src/pages/CoursesPage.tsx
+++ src/pages/CoursesPage.tsx
@@ -1,5 +1,8 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useLocation } from 'react-router-dom';
+import { app, db } from "../firebase";
+import { collection, getDocs, getDoc } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 import Announcement from "../components/Announcement"
 import StudentForum from "../components/StudentForum"
@@ -14,9 +17,71 @@ export default function CoursesPage() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const id = searchParams.get('id');
+  const name = searchParams.get('courseName');
 
   // SET STATE FOR COURSE ID
   const [courseID, setCourseID]= useState<string>(id || "")
+  const [courseName, setCourseName] = useState<string>(name || "")
+
+  const [weeklyPercentDone, setWeeklyPercentDone] = useState<number>(0);
+
+
+  // ~~~~~~~#######################~~~~~~~~~~~~~~~~ NEW NEED TO TEST THIS ~~~~~~~~~~~~~~~#############################~~~~~~~~~~~~~
+  // iterate through the user's assignments to calculate percent done for this week in this class
+  // future improvement: implementing a react context to get the courses from the dashboard page instead of having to fetch from db
+  const getWeeklyPercentDone = async () => {
+    const auth = getAuth(app);
+    onAuthStateChanged(auth, async (user) => {
+        if (user) {
+
+          const userId = user.uid;
+          const userAssignmentsRef = collection(db, `users/${userId}/assignments`);
+          const querySnapshot = await getDocs(userAssignmentsRef);
+
+          let total = 0;
+          let numCompleted = 0;
+          querySnapshot.forEach((doc) => {
+            const data = doc.data();
+            // ~~~~~~~~~~~ ********* NOT SURE IF courseName == courseID *************** ~~~~~~~~~~ 
+            // only look at assign for this course
+            console.log("courseName: " + data.courseName)
+            console.log("courseID: " + courseName)
+
+            if (data.courseName == courseName) {
+              if (data.completed) {
+
+                numCompleted = numCompleted + 1;
+              }
+              total = total + 1;
+            }
+          });
+
+          let percent: number;
+          if (total == 0) {
+            percent = 100;
+          }
+          else {
+            // maybe we should round
+            console.log("done " + numCompleted)
+            console.log("total " + numCompleted)
+            percent = (numCompleted/total) * 100;
+          }
+
+          
+          setWeeklyPercentDone(percent);
+          console.log("percent " + percent);
+        }
+      else {
+        console.log("User is not signed in");
+      }
+    });
+  };
+
+  // ~~~~~~~~~~~ NEW ~~~~~~~~~~~~~~
+  useEffect(() => {
+      getWeeklyPercentDone();
+  });
+
 
   return (
     <div className="flex flex-col relative">
@@ -25,7 +90,7 @@ export default function CoursesPage() {
       <div className="grid grid-cols-1 grid-rows-5 gap-4 sm:grid-cols-2 sm:grid-rows-3 md:grid-cols-8 md:grid-rows-2 p-6 bg-gradient-custom md:h-[calc(100vh-5rem)]" >
         <Announcement courseID={courseID}/>
         <Document/>
-        <ProgressBar/>
+        <ProgressBar weeklyPercent = {weeklyPercentDone}/>
         <Assignment/>
         <StudentForum/>     
       </div> 
