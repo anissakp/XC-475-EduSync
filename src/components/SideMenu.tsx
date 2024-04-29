7ee49c257e3bab62b560f820c5ba2f564fececeb
Anissa Patel <anissakp@bu.edu>
2024-04-29 12:06:46 -0400

    side menu

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -61,6 +61,13 @@ const SideMenu: React.FC<Props> = ({ classNameList }: Props) => {
                         buttonStyle={{ backgroundColor: '#FBE6B7', fontSize: '16px', padding: "8px 16px", border: 'none', display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: "240px", textAlign: 'left' }}
                         listStyle={{ listStyle: 'none' }}
                     listItemStyle={{ padding: '10px', cursor: "pointer", paddingLeft: '58px' }} />*/}
+
+                    <button onClick={goToPiazza} style={{ backgroundColor: '#FBE6B7', fontSize: '16px', padding: "8px 16px", border: 'none', display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: "240px", textAlign: 'left', fontFamily: 'quicksand' }}>
+                        <img src={piazzaIcon} alt="Tasks" style={{ marginRight: "7px", filter: "grayscale(100%)", width: "25px", height: "auto"}} />
+                        <span style={{ flex: '1' }}>
+                            Announcements
+                        </span>
+                    </button>
                         
                     <button onClick={goToTasks} style={{ backgroundColor: '#FBE6B7', fontSize: '16px', padding: "8px 16px", border: 'none', display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: "240px", textAlign: 'left', fontFamily: 'quicksand' }}>
                         <img src={tasksIcon} alt="Tasks" style={{ marginRight: "9px" }} />
